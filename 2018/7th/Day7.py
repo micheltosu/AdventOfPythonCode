9020d65fd3a2efdb3a0da9beda1329119ad357cb
Michel Tosu <michel@tosu.se>
2019-11-04 12:47:12 +0100

    solve: 2018 D7T2

--- 2018/7th/Day7.py
+++ 2018/7th/Day7.py
@@ -1,12 +1,12 @@
 import re
 
-# Setup and read input
-input_file = open('input.txt')
+# Setup and read test
+test_file = open('input.txt')
 
 nodes = set()
 edges = set()
 
-for line in input_file:
+for line in test_file:
     edge = re.match(r'Step (.) \w+ \w+ \w+ \w+ step (.) \w+ \w+\.', line).group(1,2)
 
     nodes.add(edge[0])
@@ -23,7 +23,8 @@ def generate_candidates(node_list, edge_list, filtered_nodes):
             c_list.remove(edge[1])
 
     for f in filtered_nodes:
-        c_list.remove(f)
+        if f in node_list:
+            c_list.remove(f)
 
     return c_list
 
@@ -45,3 +46,66 @@ while len(candidates) != 0:
     candidates = generate_candidates(nodes, edge_list,finished)
 
 print('The assembling order is: {0}'.format(''.join(finished)))
+
+# Calculate parallel timed
+def time_tasks(tlist):
+    tmap = dict()
+    for t in tlist:
+        tmap[t] = tasktime(t)
+    return tmap
+
+def tasktime(task):
+    return (ord(task) - 4)
+
+idle_workers = [i for i in range(5)]
+t2_nodes = nodes.copy()
+task_restrictions = sorted(edges, key=lambda e: e[1])
+finished_tasks = []
+tasks_in_progress = dict()
+task_assignment = dict()
+max_time = 0
+for n in nodes:
+    max_time += tasktime(n)
+
+for s in range(max_time):
+    available_tasks = generate_candidates(t2_nodes, task_restrictions, finished_tasks)  
+    timed_tasks = time_tasks(available_tasks)
+    if len(tasks_in_progress) == 0 and len(timed_tasks) == 0:
+        print('All tasks performed in {0} minutes'.format(s))
+        break
+    
+    # Assign workers
+    for task in timed_tasks:
+        if len(idle_workers) != 0:
+            task_assignment[task] = idle_workers.pop()
+            tasks_in_progress[task] = timed_tasks[task]
+            print('{2}: Assign {0} to task {1}'.format(task_assignment[task], task, s))
+
+    for task in tasks_in_progress:
+        if task in t2_nodes:
+            t2_nodes.remove(task)
+
+    # Perform work
+    finished = []
+    for task in tasks_in_progress:
+        tasks_in_progress[task] -= 1
+        if tasks_in_progress[task] == 0:
+            finished.append(task)
+            print('{1}: Task {0} finished.'.format(task, s))
+
+    # Free up finished
+    for t in finished: 
+        idle_workers.append(task_assignment.pop(t))
+        tasks_in_progress.pop(t)
+        finished_tasks.append(t)
+
+    # Remove restrictions
+    for r in task_restrictions.copy():
+        if r[0] in finished:
+            task_restrictions.remove(r)
+
+
+
+
+    
+
