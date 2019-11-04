7098735ed288554b53bc45a28bf506e5ba6cc5e9
Michel Tosu <michel@tosu.se>
2019-11-04 10:07:17 +0100

    solve: 2018 D7T1

--- 2018/7th/Day7.py
+++ 2018/7th/Day7.py
@@ -1,7 +1,7 @@
 import re
 
 # Setup and read input
-input_file = open('test.txt')
+input_file = open('input.txt')
 
 nodes = set()
 edges = set()
@@ -15,3 +15,33 @@ for line in input_file:
 
 print(nodes, edges)
 
+# Find path
+def generate_candidates(node_list, edge_list, filtered_nodes):
+    c_list = sorted(node_list)
+    for edge in edge_list:
+        if edge[1] in c_list:
+            c_list.remove(edge[1])
+
+    for f in filtered_nodes:
+        c_list.remove(f)
+
+    return c_list
+
+edge_list = sorted(edges, key=lambda e: e[1])
+finished = list()
+candidates = generate_candidates(nodes, edge_list, finished)
+
+while len(candidates) != 0:
+    c = candidates[0]
+    finished.append(c)
+    remove_edges = list()
+    for e in edge_list:
+        if c in e:
+            remove_edges.append(e)
+
+    for e in remove_edges:
+        edge_list.remove(e)
+
+    candidates = generate_candidates(nodes, edge_list,finished)
+
+print('The assembling order is: {0}'.format(''.join(finished)))
