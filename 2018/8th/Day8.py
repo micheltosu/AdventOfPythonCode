1808ab1d092a04f28bf61e5a6213fca31e66204d
Michel Tosu <michel@tosu.se>
2019-11-05 20:57:00 +0100

    solve: 2018 D8T2

--- 2018/8th/Day8.py
+++ 2018/8th/Day8.py
@@ -33,31 +33,37 @@ class node:
             meta_sum += child.sum_metadata()
 
         return meta_sum
+    def sum_nodevalue(self):
+        total = 0
 
+        if len(self.children) == 0:
+            total += sum(self.metadata)
+        else:
+            for i in self.metadata:
+                if i == 0:
+                    print('Metadata with value 0, ignoring')
+                if not i - 1 >= len(self.children):
+                    total += self.children[i - 1].sum_nodevalue()
+        return total
+
+# Reading the input
 input_arr = open('input.txt').read().strip().split(' ')
 queue = queue.SimpleQueue()
 for num in input_arr:
     queue.put(int(num))
 
-print('There are {0} items in input'.format(queue.qsize()))
 # Parse nodes
 def parse_node(que, lvl = 0):
     new_node = node(que.get(), que.get(), lvl)
     tabs = '-' * lvl
-    print(tabs + 'create node {2}: {0} child, {1} meta, level {4}, queue left:{3}'
-            .format(
-                new_node.child_qty,
-                new_node.metadata_qty, 
-                new_node.name,
-                que.qsize(),
-                lvl))
     for child in range(new_node.child_qty):
         new_node.add_child(parse_node(que, lvl + 1))
     for metadata in range(new_node.metadata_qty):
-        new_node.add_metadata(que.get())
+        new_node.add_metadata(int(que.get()))
 
     return new_node
 
 root = parse_node(queue)
 print(root)
 print('Meta sum: {0}'.format(root.sum_metadata()))
+print('Root node value: {0}'.format(root.sum_nodevalue()))
