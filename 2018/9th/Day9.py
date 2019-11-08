f31e7eb6e530ad26c3af89bed387d6a6c4d6f4ce
Michel Tosu <michel@tosu.se>
2019-11-08 10:45:53 +0100

    solve: 2018 D9T2
    Replacing the obvious inefficient solution with a circular structure.
    That one doesn't have to calculate anything but only re-link objects.

--- 2018/9th/Day9.py
+++ 2018/9th/Day9.py
@@ -1,100 +1,77 @@
 # Puzzle input: 447 players; last marble is worth 71510 points
-
-class Marble_List:
-    class Node:
-        def __init__(self, value):
-            self.next_node = None
+# Puzzle input: 447 players; last marble is worth 7151000 points
+class Marble_circle:
+    class Node: 
+        def __init__(self,value):
+            self.next = None
+            self.prev = None
             self.value = value
-
         def __str__(self):
-            string = '({0})'.format(self.value)
-            if self.next_node is not None:
-                string += '{0}'.format(self.get_next())
-
-            return string
-
-        def insert(self, new_node):
-                old_next = self.next_node
-                self.next_node = new_node
-                new_node.set_next(old_next)
-
-        def remove_next(self):
-            self.next_node = self.next_node.get_next()
-
-        def get_next(self):
-            return self.next_node
-
-        def set_next(self, next_node):
-            self.next_node = next_node
+            return '({0})'.format(self.value)
 
     def __init__(self):
-        self.length = 0
         self.head = None
-        self.current_position = 0;
+        self.first = None
+        self.length = 0
+    def __str__(self):
+        string = ''
+        print_node = self.first
+        for i in range(self.length):
+            string += str(print_node)
+            print_node = print_node.next
+            
+        return string
 
     def add(self, value):
-        if self.length == 0:
-            self.head = self.Node(value)
-        else:  
-            self.__update_current_position__()
-            parent_node = self.__get_node__(self.current_position - 1)
-            parent_node.insert(self.Node(value))
-
         self.length += 1
-    def get(self, pos):
-        return self.__get_node__(pos).value
+        new_node = self.Node(value) 
 
+        if self.head is None:
+            new_node.next = new_node
+            new_node.prev = new_node
+            self.head = new_node
+            self.first = new_node
+        else: 
+            self.head = self.head.next # placing head at correct place
+            old_head = self.head
 
-    def remove(self, position):
-        parent_node = self.__get_node__(position - 1)
-        parent_node.remove_next()
+            new_node.next = old_head.next
+            new_node.prev = old_head
+            old_head.next = new_node
+            new_node.next.prev = new_node
+
+            self.head = new_node
+            
+    def remove(self):
+        n = self.head
+        for i in range(7):
+            n = n.prev
+
+        n.prev.next = n.next
+        n.next.prev = n.prev
+        self.head = n.next
         self.length -= 1
 
-        self.current_position = position
+        if n is self.first:
+            self.first = n.next
+
+        return n.value
 
 
-
-    def __get_node__(self, pos):
-        node = self.head
-        for i in range(pos):
-            node = node.get_next()
-
-        return node
-    def get_current_position(self):
-        return self.current_position
-
-        
-
-    def __str__(self):
-        return str(self.head)
-    def __update_current_position__(self):
-        if self.length == 1:
-            self.current_position = 1
-        else:
-            nextpos = self.current_position + 2
-            if nextpos > self.length:
-                nextpos = nextpos % self.length
-
-            self.current_position = nextpos
-
-
-
-
-def play_game(player_count, marbles):
-    game_list = Marble_List()
+def play_fast(player_count, marbles):
+    game_list = Marble_circle()
     players = [0] * player_count
     current_player = 0
     
     for i in range(marbles + 1):
         if i > 22 and i % 23 == 0:
-            to_remove = game_list.get_current_position() - 7
-            players[current_player] += (i + game_list.get(to_remove))
-            game_list.remove(to_remove)
+            score = game_list.remove()
+            score += i
+            players[current_player] += score
         else: 
             game_list.add(i)
 
         current_player = (current_player + 1) % player_count
-#        print(game_list)
 
     header = ''
     scores = ''
@@ -103,17 +80,19 @@ def play_game(player_count, marbles):
         scores += '| {0} '.format(players[i])
 
 
-    print('Game finished, player scores are:')
-    print(header)
-    print(scores)
+#   print('Game finished, player scores are:')
+#   print(header)
+#   print(scores)
     print('Highscore: {0}'.format(max(players)))
 
-play_game(9, 25)
-play_game(10, 1618)
-play_game(13, 7999)
-play_game(17, 1104)
-play_game(21, 6111)
-play_game(30, 5807)
-play_game(447, 71510)
-            
-
+# Some test input from the puzzle description.
+play_fast(9, 25)
+play_fast(10, 1618)
+play_fast(13, 7999)
+play_fast(17, 1104)
+play_fast(21, 6111)
+play_fast(30, 5807)
+# Solve puzzle 1
+play_fast(447, 71510)
+# Solve puzzle 2            
+play_fast(447,7151000)
