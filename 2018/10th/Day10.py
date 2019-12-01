542cb6e7fab51f88ef7a704e216cbaedda86d000
Michel Tosu <michel@tosu.se>
2019-12-01 07:08:13 +0100

    work in progress

---
+++ 2018/10th/Day10.py
@@ -0,0 +1,34 @@
+import io
+
+in_file = open("test.txt");
+
+for line in in_file:
+    print(line.strip())
+
+
+
+'''
+list with tuple(tuple, tuple)
+sort list on first tuple
+
+for each 
+
+
+
+Create class containing:
+    position
+    velocity
+
+    functions
+    =========
+    update position
+    representation
+
+
+Create class for sky:
+    list of stars
+
+    functions
+    ==========
+    read stars
+
