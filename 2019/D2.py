490ae6bb03b2d20d2f02b58b9dd6e4c44d8e8ff9
Michel Tosu <michel@tosu.se>
2019-12-02 07:06:59 +0100

    solve: 2019D2T2

--- 2019/D2.py
+++ 2019/D2.py
@@ -1,28 +1,55 @@
 opcodes = open("D2-input.txt").read().strip().split(',')
 for i in range(len(opcodes)):
     opcodes[i] = int(opcodes[i])
-print(opcodes)
 
+def copy_memory():
+    global opcodes
+    res = opcodes.copy() 
+    return res
+
+memory = copy_memory()
+print(memory)
 
 #program loop
-opcodes[1] = 12
-opcodes[2] = 2
 PC = 0
 def forward():
     global PC 
     PC += 4
+def run_program(a1, a2):
+    global PC 
+    global memory
+    PC = 0
+    memory[1] = a1
+    memory[2] = a2
 
-while True:
-    a1, a2, a3 = opcodes[PC+1:PC+4]
-    if opcodes[PC] == 1:
-        print("Opcode 1")
-        opcodes[a3] = opcodes[a1] + opcodes[a2]
-    elif opcodes[PC] == 2:
-        print("Opcode 2")
-        opcodes[a3] = opcodes[a1] * opcodes[a2]
-    elif opcodes[PC] == 99:
-        print("End program")
+    while True:
+        if memory[PC] == 99:
+            print("End program")
+            return memory[0]
+        a1, a2, a3 = memory[PC+1:PC+4]
+
+        if memory[PC] == 1:
+            print("Opcode 1")
+            memory[a3] = memory[a1] + memory[a2]
+        elif memory[PC] == 2:
+            print("Opcode 2")
+            memory[a3] = memory[a1] * memory[a2]
+        forward()
+
+
+run_program(12,2)
+
+out = 0
+# Task 2
+for i in range(len(opcodes) - 1):
+    for j in range(len(opcodes) - 1):
+        print('Run with {0}, {1}'.format(i, j))
+        memory = copy_memory()
+        out = run_program(i, j)
+
+        if out == 19690720:
+            print('verb: {0}, noun: {1}'.format(i, j))
+            break
+    if out == 19690720:
         break
 
-    forward()
-print("Position 0:{0}".format(opcodes[0]))
