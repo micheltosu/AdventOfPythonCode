aea639f725a0b2ad238b276b2ba246d1d1ae4b60
Michel Tosu <michel@tosu.se>
2019-12-02 07:19:03 +0100

    refactor: 2019D2

--- 2019/D2.py
+++ 2019/D2.py
@@ -1,49 +1,49 @@
+# Read the input for the day
 opcodes = open("D2-input.txt").read().strip().split(',')
 for i in range(len(opcodes)):
     opcodes[i] = int(opcodes[i])
 
+# Function that returns a fresh copy of the program from 'disk'
 def copy_memory():
     global opcodes
     res = opcodes.copy() 
     return res
 
-memory = copy_memory()
-print(memory)
-
-#program loop
 PC = 0
+# Procedure to increment program counter to next instruction
 def forward():
     global PC 
     PC += 4
+
+# Runs the program given by copy_memory
 def run_program(a1, a2):
     global PC 
-    global memory
+    memory = copy_memory()
     PC = 0
     memory[1] = a1
     memory[2] = a2
 
     while True:
         if memory[PC] == 99:
-            print("End program")
             return memory[0]
+
+        # Load the arguments
         a1, a2, a3 = memory[PC+1:PC+4]
 
         if memory[PC] == 1:
-            print("Opcode 1")
             memory[a3] = memory[a1] + memory[a2]
         elif memory[PC] == 2:
-            print("Opcode 2")
             memory[a3] = memory[a1] * memory[a2]
         forward()
 
+#Task 1
+print('The result for verb 12, noun 2: ', run_program(12,2))
 
-run_program(12,2)
-
-out = 0
 # Task 2
+print('Finding out which verb and noun gives 19690720')
+out = 0
 for i in range(len(opcodes) - 1):
     for j in range(len(opcodes) - 1):
-        print('Run with {0}, {1}'.format(i, j))
         memory = copy_memory()
         out = run_program(i, j)
 
