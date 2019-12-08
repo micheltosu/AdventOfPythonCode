38e55f0fd2ef0d65d2e066a0ee2062a3d7d910b2
Michel Tosu <michel@tosu.se>
2019-12-08 18:16:15 +0100

    solve: 2019D6T2

--- 2019/D6.py
+++ 2019/D6.py
@@ -12,12 +12,7 @@ for orb in orb_input:
     else:
        orb_map[split[0]].append(split[1])
 
-print(orb_map)
-
-print(orb_map['COM'])
-
 def find_sattelites(key, chain = 0):
-    print('find orbs for ', key)
     if key not in orb_map:
         return chain
     else:
@@ -26,5 +21,46 @@ def find_sattelites(key, chain = 0):
             sum += find_sattelites(orb, chain + 1)
         return sum
 
-print(find_sattelites('COM'))
+print('Number or direct and indirect orbits: ', find_sattelites('COM'))
+
+def has_path(fr, to):
+    path_exists = False
+    if fr in orb_map:
+        for orb in orb_map[fr]:
+            if orb == to:
+                return True
+            elif has_path(orb, to) is True:
+                path_exists = True
+
+            
+    return path_exists
+
+def find_path(fr, to, path):
+    if fr == to:
+        return path
+    elif fr not in orb_map:
+        return None
+    else:
+        path.append(fr)
+        for orb in orb_map[fr]:
+            p = find_path(orb, to, path.copy())
+            if p != None:
+                return p
+
+
+
+# Task 2
+you_path = find_path('COM','YOU', list())
+can_path = find_path('COM','SAN', list())
+same_dist = 0
+for i in range(max(len(you_path), len(can_path))):
+    if (you_path[i] == can_path[i]):
+        same_dist += 1
+    else:
+        break
+
+jumps = len(you_path) - same_dist
+jumps += len(can_path) - same_dist
+
+print('Jumps needed: ', jumps)
 
