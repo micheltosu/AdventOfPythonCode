f0d5c91b045cdb5f37d135bcacfde7fc225be2e8
Michel Tosu <michel@tosu.se>
2019-12-08 05:55:36 +0100

    refactor: 2019D7T2

--- 2019/D7.py
+++ 2019/D7.py
@@ -5,8 +5,15 @@ opcodes = open("D7-input.txt").read().strip().split(',')
 for i in range(len(opcodes)):
     opcodes[i] = int(opcodes[i])
 class Computer:
+    class NoMoreSteps(Exception):
+        pass
+    class NoInput(Exception):
+        pass
+
+
     memory = None
     PC = 0
+    read_pos = 0
     in_stream = None
     out_stream = None
 
@@ -72,7 +79,6 @@ class Computer:
 
     def output_value(self, memory, a1, pmode):
         value = self.get_param(a1, pmode[-1], memory)
-        print(value)
 
     def jump_if_true(self, memory, a1, a2, pmode):
         condition = self.get_param(a1, pmode[-1], memory)
@@ -105,13 +111,16 @@ class Computer:
             memory[a3] = 0
 
     def get_value_stream(self, memory, a1, stream, read_pos):
-        streampos = stream.tell()
-        stream.seek(read_pos)
-        val = stream.readline()
-        if val != '':
-            memory[a1] = int(val)
+        inpt = stream.getvalue().strip().split('\n')
+        if (read_pos >= len(inpt)):
+            raise Computer.NoInput('novalue')
 
-        stream.seek(streampos)
+        val = inpt[read_pos]
+        if val != '' and val != '\n':
+            memory[a1] = int(val)
+        else:
+            raise Computer.NoInput('no value')
+
 
     def output_value_stream(self, memory, a1, pmode, stream):
         stream.write(str(self.get_param(a1, pmode[-1], memory)) + '\n')
@@ -122,57 +131,79 @@ class Computer:
     #                         #
     ###########################
 
+    def __perform_cycle__(self):
+        memory = self.memory
+        i = ''.join(self.in_stream.getvalue().split('\n'))
+        o = ''.join(self.out_stream.getvalue().split('\n'))
+
+        p_modes = self.parse_param_modes(memory[self.PC]) 
+        opcode = self.parse_opcode(memory[self.PC])
+        # Load the arguments
+
+        if opcode == 99:
+            print("Should exit")
+            raise Computer.NoMoreSteps('At program end')
+
+        elif opcode == 1:
+            a1, a2, a3 = memory[self.PC+1:self.PC+4]
+            self.add(memory, a1, a2, a3, p_modes)
+            self.PC += 4
+        elif opcode == 2:
+            a1, a2, a3 = memory[self.PC+1:self.PC+4]
+            self.mul(memory, a1, a2, a3, p_modes)
+            self.PC += 4
+        elif opcode == 3:
+            a1 = memory[self.PC+1]
+            try:
+                self.get_value_stream(memory, a1, self.in_stream, self.read_pos)
+                self.read_pos += 1
+                self.PC += 2
+            except Computer.NoInput:
+                return
+        elif opcode == 4:
+            a1 = memory[self.PC+1]
+            self.output_value_stream(memory, a1, p_modes, self.out_stream)
+            self.PC += 2
+        elif opcode == 5:
+            a1, a2, a3 = memory[self.PC+1:self.PC+4]
+            res = self.jump_if_true(memory, a1, a2, p_modes)
+            if res != 0:
+                self.PC = res
+            else:
+                self.PC += 3
+        elif opcode == 6:
+            a1, a2, a3 = memory[self.PC+1:self.PC+4]
+            res = self.jump_if_false(memory, a1, a2, p_modes)
+            if res != 0:
+                self.PC = res
+            else:
+                self.PC += 3
+        elif opcode == 7:
+            a1, a2, a3 = memory[self.PC+1:self.PC+4]
+            self.less_than(memory, a1, a2, a3, p_modes)
+            self.PC += 4
+        elif opcode == 8:
+            a1, a2, a3 = memory[self.PC+1:self.PC+4]
+            self.equals(memory, a1, a2, a3, p_modes)
+            self.PC += 4
+        else:
+            raise Computer.NoMoreSteps('At program end')
+
+    def step(self):
+        self.__perform_cycle__()
+
     # Runs the program from start to end
     def run_program(self, *args):
-        memory = self.memory
 
         arg_arr = [str(args[i]) for i in range(len(args))]
         self.in_stream.write('\n'.join(arg_arr))
         
-        read_pos = 0
 
-        while memory[self.PC] != 99:
-            p_modes = self.parse_param_modes(memory[self.PC]) 
-            opcode = self.parse_opcode(memory[self.PC])
-            # Load the arguments
-            a1, a2, a3 = memory[self.PC+1:self.PC+4]
-
-            if opcode == 1:
-                self.add(memory, a1, a2, a3, p_modes)
-                self.PC += 4
-            elif opcode == 2:
-                self.mul(memory, a1, a2, a3, p_modes)
-                self.PC += 4
-            elif opcode == 3:
-                self.get_value_stream(memory, a1, self.in_stream, read_pos)
-                read_pos+=2
-                print("read {0} from in stream".format(memory[a1]))
-                self.PC += 2
-            elif opcode == 4:
-                print("put {0} in out stream".format(memory[a1]))
-                self.output_value_stream(memory, a1, p_modes, self.out_stream)
-                self.PC += 2
-            elif opcode == 5:
-                res = self.jump_if_true(memory, a1, a2, p_modes)
-                if res != 0:
-                    self.PC = res
-                else:
-                    self.PC += 3
-            elif opcode == 6:
-                res = self.jump_if_false(memory, a1, a2, p_modes)
-                if res != 0:
-                    self.PC = res
-                else:
-                    self.PC += 3
-            elif opcode == 7:
-                self.less_than(memory, a1, a2, a3, p_modes)
-                self.PC += 4
-            elif opcode == 8:
-                self.equals(memory, a1, a2, a3, p_modes)
-                self.PC += 4
+        while self.memory[self.PC] != 99:
+            self.__perform_cycle__()
 
         if (len(self.out_stream.getvalue()) == 0):
-            return memory[0]
+            return self.memory[0]
         else:
             return int(self.out_stream.getvalue())
 
@@ -191,7 +222,7 @@ def phase_test():
         signals.append(signal)
 
     return max(signals)
-print(phase_test())
+#print(phase_test())
 
 # Task 2
 def feedback_test():
@@ -200,26 +231,40 @@ def feedback_test():
 
     for phase in phases:
         streams = [io.StringIO() for _ in range(5)]
+        computers = [Computer(opcodes) for _ in range(5)]
         for p in range(5):
             print ('Setup stream {0} with phase {1}'.format(p, phase[p]))
             streams[p].write(str(phase[p]) + '\n')
             if p == 4:
                 streams[p].write(str(0) + '\n')
-        for p in range(5):
-            in_stream = (p + 4) % 5
-            signal = run_program(streams[in_stream], streams[p])
-        streams[4].seek(streams[4].tell() - 2)
-        output = streams[4].readline()
-        signals.append(int(output))
+
+        for com in range(5):
+            in_stream = (com + 4) % 5
+            computers[com].set_in_stream(streams[in_stream])
+            computers[com].set_out_stream(streams[com])
+
+
+        running_computers = computers.copy()
+        running = 5
+        while running > 0:
+            for c in running_computers:
+                try:
+                    c.step()
+                except Computer.NoMoreSteps:
+                    running_computers.remove(c)
+                    running -= 1
+
+        print("Done.")
+        
+
+
+        out_arr = streams[4].getvalue().strip().split('\n')
+        out_val = out_arr[len(out_arr) - 1]
+        signals.append(int(out_val))
 
     return max(signals)
-in_stream = io.StringIO()
-in_stream.write('\n'.join(str(x) for x in [9,0]))
-out_stream = io.StringIO()
-run_program(in_stream, out_stream)
-print(out_stream.getvalue())
 
-feedback_test()
+print(feedback_test())
 
 
 
