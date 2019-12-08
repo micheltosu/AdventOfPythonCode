42bb7ef075ffece81396bf90116c26a1a4e6360d
Michel Tosu <michel@tosu.se>
2019-12-08 08:53:56 +0100

    refactor: cleanup

--- 2019/D8.py
+++ 2019/D8.py
@@ -3,23 +3,17 @@ pixel_data = open("D8-input.txt").read().strip()
 width = 25
 height = 6
 row_length = width * height
-print(len(pixel_data))
 
-layers = [[] for i in range(len(pixel_data) // (width * height))]
-print("Number of layers", len(layers))
+layers = [[] for i in range(len(pixel_data) // row_length)]
+print("Number of layers: ", len(layers))
 
 for l in range(len(layers) ):
-    for pos in range( width * height ):
-        layers[l].append(pixel_data[(l*row_length) + pos])
-
-
-
+    for pos in range(row_length):
+        layers[l].append(pixel_data[l * row_length + pos])
 
 counts = list()
 for list in layers:
-    ones = 0
-    twos = 0
-    zeros = 0
+    ones, twos, zeros = [0,0,0]
 
     for raw in list:
         val = int(raw)
@@ -32,24 +26,25 @@ for list in layers:
 
     counts.append((zeros, ones, twos))
 
-smallest_zero_count = None
-count = None
+least_zeros = None
+zeros_count = None
 for layer in counts:
     if (layer[0] + layer[1] + layer[2]) != 150:
         print(layer, "is not 150 in size")
-    if count == None or layer[0] < count:
-        smallest_zero_count = layer
-        count = layer[0]
+    if zeros_count == None or layer[0] < zeros_count:
+        least_zeros = layer
+        zeros_count = layer[0]
 
-print(smallest_zero_count)
-print(smallest_zero_count[1] * smallest_zero_count[2])
+print('The digit count for layer with least amount of zeros (0, 1, 2): ', least_zeros)
+print('Count of ones multiplied with count of twos: ', least_zeros[1] * least_zeros[2])
 
 # Task 2
+print('Rendering image:\n')
 image = [[2] * width for i in range(height)]
 for layer in layers:
     for h in range(height):
         for w in range(width):
-            layer_pos = (h*width) + w 
+            layer_pos = h * width + w 
             char = int(layer[layer_pos])
             if image[h][w] == 2:
                 image[h][w] = char
@@ -57,34 +52,10 @@ for layer in layers:
 for row in image:
     for pos in row:
         char = ' '
-        if pos == 1:
-            char = ' '
-        elif pos == 0: 
-            char = 'X'
+        if pos == 1: 
+            char = '#'
         print(char, end='')
     print()
             
-image2 = [[2] * width for i in range(height)]
-for h in range(height):
-    for w in range(width):
-        layer_pos = (h*width) + w 
-        for layer in layers:
-            pixel = int(image2[h][w])
-            if pixel == 2:
-                print("replace pixel with:",layer[layer_pos])
-                image2[h][w] = int(layer[layer_pos])
-
-print(image2)
-
-
-for row in image2:
-    for pos in row:
-        char = ' '
-        if pos == 1:
-            char ='X'
-        elif pos == 0: 
-            char = ' '
-        print(char, end='')
-    print()
 
 
